1ef63ae60b3c16d13b6b77fa85883bc879506fa2
David407407 <darksideinthespace@gmail.com>
2024-02-29 19:02:30 -0600

    titulo

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import { jetbrains } from "./layout";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="bg-[#2D3440] w-full min-h-screen">
+      <h1 className={`${jetbrains.className} text-5xl text-center font-bold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent bg-clip-text leading-normal`}>Art.jsasdasdsd</h1>
     </main>
   );
 }
